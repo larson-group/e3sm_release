f4bc960c6ff8851652d4126ebc540c462a3160ff
zhunguo <48636672+zhunguo@users.noreply.github.com>
2021-05-09 16:56:06 +0800

    Update micro_p3.F90
    We set Vqit=Vqit*0.4, Vnit=Vnit*0.4. Result can be found in run [E](https://web.lcrc.anl.gov/public/e3sm/diagnostic_output/ac.zguo/P3_0504_ANN/2D.htm).

--- components/eam/src/physics/cam/micro_p3.F90
+++ components/eam/src/physics/cam/micro_p3.F90
@@ -4166,8 +4166,8 @@ subroutine ice_sedimentation(kts,kte,ktop,kbot,kdir,    &
 
 #ifdef SILHS
          ! Output V_qit for use in SILHS hole filler.
-         V_qit = V_qit*0.5
-         V_nit = V_nit*0.5
+         V_qit = V_qit*0.4
+         V_nit = V_nit*0.4
          if ( dt_left == dt ) then
             V_qi_out = V_qit
          endif
